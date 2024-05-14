261690b8613c47a9c852a0cf22afd1e47360ef70
Sanlh <115389441+Sanlh@users.noreply.github.com>
2024-05-14 08:58:22 +0700

    Add files via upload

---
+++ BUS/ChiTietHoaDonBUS.cs
@@ -0,0 +1,30 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BUS
+{
+    public class ChiTietHoaDonBUS
+    {
+        ChiTietHoaDonDAO chiTietHoaDonDAO = new ChiTietHoaDonDAO();
+        public bool ThemChiTietHoaDon(ChiTietHoaDon cthd)
+        {
+            return chiTietHoaDonDAO.ThemChiTietHoaDon(cthd);
+        }
+        public List<string> ChiTietHoaDon(int mahoadon)
+        {
+            return chiTietHoaDonDAO.ChiTietHoaDon(mahoadon);
+        }
+        public int SoLuongHangDaBan()
+        {
+            return chiTietHoaDonDAO.SoLuongSanPhamDaBan();
+        }
+        public int SoLuongSanPhamDaBanTheoThoiGian(string ngaylap1, string ngaylap2)
+        {
+            return chiTietHoaDonDAO.SoLuongSanPhamDaBanTheoThoiGian(ngaylap1, ngaylap2);
+        }
+    }
+}
